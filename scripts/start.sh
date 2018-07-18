90b3a43c100579254bcffdbe093d68db83a440ba
Rifky Ekayama <rifqykiw@gmail.com>
2018-07-18 18:43:59 +0700

    fixing images

---
+++ scripts/start.sh
@@ -0,0 +1,157 @@
+#!/bin/bash
+
+# Disable Strict Host checking for non interactive git clones
+
+envsubst < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
+
+mkdir -p -m 0700 /root/.ssh
+# Prevent config files from being filled to infinity by force of stop and restart the container 
+echo "" > /root/.ssh/config
+echo -e "Host *\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
+
+if [[ "$GIT_USE_SSH" == "1" ]] ; then
+  echo -e "Host *\n\tUser ${GIT_USERNAME}\n\n" >> /root/.ssh/config
+fi
+
+if [ ! -z "$SSH_KEY" ]; then
+ echo $SSH_KEY > /root/.ssh/id_rsa.base64
+ base64 -d /root/.ssh/id_rsa.base64 > /root/.ssh/id_rsa
+ chmod 600 /root/.ssh/id_rsa
+fi
+
+# Set custom webroot
+if [ ! -z "$WEBROOT" ]; then
+ sed -i "s#root /var/www/html;#root ${WEBROOT};#g" /etc/nginx/conf.d/default.conf
+else
+ webroot=/var/www/html
+fi
+
+# Setup git variables
+if [ ! -z "$GIT_EMAIL" ]; then
+ git config --global user.email "$GIT_EMAIL"
+fi
+if [ ! -z "$GIT_NAME" ]; then
+ git config --global user.name "$GIT_NAME"
+ git config --global push.default simple
+fi
+
+# Dont pull code down if the .git folder exists
+if [ ! -d "/var/www/html/.git" ]; then
+ # Pull down code from git for our site!
+ if [ ! -z "$GIT_REPO" ]; then
+   # Remove the test index file if you are pulling in a git repo
+   if [ ! -z ${REMOVE_FILES} ] && [ ${REMOVE_FILES} == 0 ]; then
+     echo "skiping removal of files"
+   else
+     rm -Rf /var/www/html/*
+   fi
+   GIT_COMMAND='git clone '
+   if [ ! -z "$GIT_BRANCH" ]; then
+     GIT_COMMAND=${GIT_COMMAND}" -b ${GIT_BRANCH}"
+   fi
+
+   if [ -z "$GIT_USERNAME" ] && [ -z "$GIT_PERSONAL_TOKEN" ]; then
+     GIT_COMMAND=${GIT_COMMAND}" ${GIT_REPO}"
+   else
+    if [[ "$GIT_USE_SSH" == "1" ]]; then
+      GIT_COMMAND=${GIT_COMMAND}" ${GIT_REPO}"
+    else
+      GIT_COMMAND=${GIT_COMMAND}" https://${GIT_USERNAME}:${GIT_PERSONAL_TOKEN}@${GIT_REPO}"
+    fi
+   fi
+   ${GIT_COMMAND} /var/www/html || exit 1
+   if [ ! -z "$GIT_TAG" ]; then
+     git checkout ${GIT_TAG} || exit 1
+   fi
+   if [ ! -z "$GIT_COMMIT" ]; then
+     git checkout ${GIT_COMMIT} || exit 1
+   fi
+   if [ -z "$SKIP_CHOWN" ]; then
+     chown -Rf nginx.nginx /var/www/html
+   fi
+ fi
+fi
+
+# Enable custom nginx config files if they exist
+if [ -f /var/www/html/conf/nginx/nginx.conf ]; then
+  cp /var/www/html/conf/nginx/nginx.conf /etc/nginx/nginx.conf
+fi
+
+if [ -f /var/www/html/conf/nginx/nginx-site.conf ]; then
+  cp /var/www/html/conf/nginx/nginx-site.conf /etc/nginx/conf.d/default.conf
+fi
+
+if [ -f /var/www/html/conf/nginx/nginx-site-ssl.conf ]; then
+  cp /var/www/html/conf/nginx/nginx-site-ssl.conf /etc/nginx/conf.d/default-ssl.conf
+fi
+
+# Pass real-ip to logs when behind ELB, etc
+if [[ "$REAL_IP_HEADER" == "1" ]] ; then
+ sed -i "s/#real_ip_header X-Forwarded-For;/real_ip_header X-Forwarded-For;/" /etc/nginx/conf.d/default.conf
+ sed -i "s/#set_real_ip_from/set_real_ip_from/" /etc/nginx/conf.d/default.conf
+ if [ ! -z "$REAL_IP_FROM" ]; then
+  sed -i "s#172.16.0.0/12#$REAL_IP_FROM#" /etc/nginx/conf.d/default.conf
+ fi
+fi
+# Do the same for SSL sites
+if [ -f /etc/nginx/conf.d/default-ssl.conf ]; then
+ if [[ "$REAL_IP_HEADER" == "1" ]] ; then
+  sed -i "s/#real_ip_header X-Forwarded-For;/real_ip_header X-Forwarded-For;/" /etc/nginx/conf.d/default-ssl.conf
+  sed -i "s/#set_real_ip_from/set_real_ip_from/" /etc/nginx/conf.d/default-ssl.conf
+  if [ ! -z "$REAL_IP_FROM" ]; then
+   sed -i "s#172.16.0.0/12#$REAL_IP_FROM#" /etc/nginx/conf.d/default-ssl.conf
+  fi
+ fi
+fi
+
+if [ ! -z "$PUID" ]; then
+  if [ -z "$PGID" ]; then
+    PGID=${PUID}
+  fi
+  deluser nginx
+  addgroup -g ${PGID} nginx
+  adduser -D -S -h /var/cache/nginx -s /sbin/nologin -G nginx -u ${PUID} nginx
+else
+  if [ -z "$SKIP_CHOWN" ]; then
+    chown -Rf nginx.nginx /var/www/html
+  fi
+fi
+
+# Run custom scripts
+if [[ "$RUN_SCRIPTS" == "1" ]] ; then
+  if [ -d "/var/www/html/scripts/" ]; then
+    # make scripts executable incase they aren't
+    chmod -Rf 750 /var/www/html/scripts/*; sync;
+    # run scripts in number order
+    for i in `ls /var/www/html/scripts/`; do /var/www/html/scripts/$i ; done
+  else
+    echo "Can't find script directory"
+  fi
+fi
+
+# Yarn install
+if [ "$YARN_INSTALL" == "1" ]; then
+  cd /var/www/html
+  yarn 
+  cd ~/
+fi
+
+if [ ! -z "$YARN_BUILD_COMMAND" ]; then
+  cd /var/www/html
+  $YARN_BUILD_COMMAND
+  cd ~/
+fi
+
+yarn global add pm2@latest
+
+if [ ! -z "$YARN_RUN_COMMAND" ]; then
+  cd /var/www/html
+  pm2 start "$YARN_RUN_COMMAND"
+  cd ~/
+fi
+
+# nginx
+nginx -s reload
+
+# Start supervisord and services
+exec /usr/bin/supervisord -n -c /etc/supervisord.conf
